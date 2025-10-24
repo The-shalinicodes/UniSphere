a5df1e9bfdc6b3086a02ca396faf45263e92900f
Shalu <shlokashalini@gmail.com>
2025-10-24 09:18:01 +0530

    Final Deployment

--- fswdproj/script-cont.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "280px";
-    document.getElementById("mySidenav").style.padding = "10px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("mySidenav").style.padding = "0";
-}
